9dd47ea11c5fdb5a25362ea24eef8686f887403e
Okeanos <76732468+Okeanos-0@users.noreply.github.com>
2022-01-05 19:12:19 +0300

    allah

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,34 @@
-import { Client as DiscordTSClient } from "@typeit/discord";
-import { client } from "../../settings.json";
+import { Client as DiscordTSClient, Discord, Guild, Slash, SlashOption } from "discordx";
 import "reflect-metadata";
-import { Intents } from "discord.js";
-import Path from "path";
+import { config as _config } from "dotenv";
+_config();
+import { CommandInteraction, Intents } from "discord.js";
 
-export default Client;
+@Discord()
+abstract class App {
+    @Slash("add")
+    // @Guild("854844351556878336")
+    add(
+        @SlashOption("x", { description: "x value" })
+        x: number,
+        @SlashOption("y", { description: "y value" })
+        y: number,
+
+        interaction: CommandInteraction
+    ) {
+        interaction.reply(String(x + y));
+    }
+}
+
+const client = new DiscordTSClient({
+    botId: "",
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+client.login(process.env.TOKEN).then(() => {
+    client.initApplicationCommands();
+});
+
+client.on("interactionCreate", (interaction) => {
+    client.executeInteraction(interaction);
+});
