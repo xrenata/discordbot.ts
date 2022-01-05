c229897f078d4d3629a2c3078b9451452b464d5b
John Doe <emirhancelik@tutanota.com>
2022-01-05 18:31:01 +0300

    main

---
+++ src/client/client.ts
@@ -0,0 +1,7 @@
+import { Client as DiscordTSClient } from "@typeit/discord";
+import { client } from "../../settings.json";
+import "reflect-metadata";
+import { Intents } from "discord.js";
+import Path from "path";
+
+export default Client;
